6c870a0c44399c9d16dbe4d154bf737840e5d50f
chafek <cafir@hotmail.fr>
2022-12-30 10:32:43 +0100

    initialisation du projet picom

---
+++ src/main/java/com/aminechafek/orsys/OrsysApplication.java
@@ -0,0 +1,13 @@
+package com.aminechafek.orsys;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrsysApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrsysApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aminechafek/orsys/OrsysApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aminechafek.orsys;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrsysApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
